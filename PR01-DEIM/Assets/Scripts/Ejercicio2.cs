f1f317cea1c35317e1becdc43f909ce0429f2487
Iván <quesadagarciaiv@gmail.com>
2021-09-21 09:58:31 +0100

    Ejercicio 2
    Ejercicio 2 completo

---
+++ PR01-DEIM/Assets/Scripts/Ejercicio2.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    int n;
+    int aleatorio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        //parte b del ejercicio 2 (bucle con while y for)
+        while(n < 10)
+        {
+            n++;
+            print(n);
+        }
+        */
+
+        /*
+        //parte c del ejercicio 2 (combinación de bucles)
+        for(n = 0; n <= 10; n++)
+        {
+            print("Contador: " + n);
+            if (n >= 10)
+            {
+                print("El contador es superior a " + n);
+            }
+        }
+        */
+
+        //parte d del ejercicio 2 (generar números al azar)
+        while (aleatorio != 9)
+        {
+            print("No ha salido el 9, es " + aleatorio);
+            aleatorio = Random.Range(0, 15);
+        }
+        print("¡Ha salido el 9!");
+    }
+}
